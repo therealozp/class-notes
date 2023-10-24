c1bac6cf2dfedad9c2b28c064b51ea31c4579510
therealozp <anhkhang.le0910@gmail.com>
2023-10-24 14:32:54 -0400

    vault backup: 2023-10-24 14:32:54

--- comp-org/asm code/function_calling.asm
+++ comp-org/asm code/function_calling.asm
@@ -1,6 +1,12 @@
 .data
-	
+		a: .word 8, 1, 6, 5, 3, 4, 2, 7
+		n: .word 8
 
+.text
+	la a0, a
+	lw a1, n
+	
+j bubblesort
 swap: 
 	lw t0, 0(a0)
 	lw t1, 0(a1)
@@ -16,20 +22,21 @@ bubblesort:
 	for1: bge s0, s4, exit1
 		add s5, x0, x0 # setup boolean variable swapped to false
 		add s1, x0, x0 # initialize second loop variable j
-		sub t0, s4, s0 # initialize second loop limit n - 1 - i 
-		for2: bge s1, t0, exit2
+		sub t4, s4, s0 # initialize second loop limit n - 1 - i 
+		
+		for2: bge s1, t4, exit2
 			# address j in t1
-			slli t1, s1, 2
+			slli t5, s1, 2
 			# a0 is already added to s3
-			add t1, s3, t1
-			lw t2, 0(t1)
-			lw t3, 4(t1)
+			add t5, s3, t5
+			lw t2, 0(t5)
+			lw t3, 4(t5)
 			
 			# if arr[j+1) >= arr[j]
 			if: bge t3, t2, endif
 				# preparing arguments
-				add a0, t1, x0
-				addi a1, t1, 4
+				add a0, t5, x0
+				addi a1, t5, 4
 				# boolean = 1
 				addi s5, s5, 1
 				# jr ra swap
@@ -41,6 +48,8 @@ bubblesort:
 		
 		# break statement
 		if_outer: beq s5, x0, exit1
+		addi s0, s0, 1
+		j for1
 	exit1: addi s0, s0, 1
 	
 	
