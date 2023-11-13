3d8ba993485e26b79fa2a71b2d557e2066021d35
therealozp <anhkhang.le0910@gmail.com>
2023-11-13 12:40:21 -0500

    vault backup: 2023-11-13 12:40:20

---
+++ prog-design/code/project9_guests.c
@@ -0,0 +1,196 @@
+// Name: Phu Anh Khang Le
+// Description: A database program that uses struct to add, and list guests
+// using dynamically linked lists
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define NAME_LEN 30
+#define PHONE_LEN 20
+
+struct guest{
+	char phone[PHONE_LEN+1];
+	char last[NAME_LEN+1];
+	char first[NAME_LEN+1];
+	int party_size;
+	struct guest *next;
+};
+
+
+struct guest *add_guest(struct guest *list);
+struct guest *remove_guest(struct guest *list);
+void print_list(struct guest *list);
+void clear_list(struct guest *list);
+
+int read_line(char str[], int n);
+
+/**********************************************************
+ * main: Prompts the user to enter an operation code,     *
+ *       then calls a function to perform the requested   *
+ *       action. Repeats until the user enters the        *
+ *       command 'q'. Prints an error message if the user *
+ *       enters an illegal code.                          *
+ **********************************************************/
+int main(void)
+{
+  char code;
+
+  struct guest *new_list = NULL;  
+  printf("Operation Code: a for adding to the list at the end, r for removing from the list, p for printing the list; q for quit.\n");
+  for (;;) {
+    printf("Enter operation code: ");
+    scanf(" %c", &code);
+    while (getchar() != '\n')   /* skips to end of line */
+      ;
+    switch (code) {
+      case 'a': new_list = add_guest(new_list);
+                break;
+      case 'p': print_list(new_list);
+                break;
+      case 'r': new_list = remove_guest(new_list);
+                break;
+      case 'q': clear_list(new_list);
+		return 0;
+      default:  printf("Illegal code\n");
+    }
+    printf("\n");
+  }
+}
+
+
+struct guest *find_guest(struct guest *list, char* number) {
+  // a custom find function that returns a pointer if the searched struct exists
+  // else, just return NULL
+  struct guest *p; 
+  // iterate through the entire linked list until the NULL element
+  for (p = list; p != NULL; p = p->next) {
+    // if they are the same, then return the guest
+    if (!strcmp(number, p->phone)) {
+      return p;
+    }
+  }
+  return NULL;
+}
+
+struct guest *add_guest(struct guest *list){
+  struct guest *new_guest; 
+  // allocate some memory for the new object
+  new_guest = malloc(sizeof(struct guest));
+  // if it fails to allocate, return the original linked list along with a message
+  if (new_guest == NULL) {
+    printf("malloc failed at add_guest()");
+    return list;
+  }
+  // enter the phone number and call find() on the new guest immediately
+  printf("Enter phone number: ");
+  read_line(new_guest->phone, PHONE_LEN);
+  if (find_guest(list, new_guest->phone) != NULL) {
+    printf("guest already exists.");
+    // if a guest is found, no point in keeping it, so free it to avoid any memory leaks
+    free(new_guest);
+    // and return the head of the list
+    return list;
+  }
+  // scan in the expected values
+  new_guest->next = NULL;
+  printf("Enter guest's last name: ");
+  read_line(new_guest->last, NAME_LEN);
+  printf("Enter guest's first name: ");
+  read_line(new_guest->first, NAME_LEN);
+  printf("Enter party size: ");
+  scanf("%d", &new_guest->party_size);
+
+  // check if it's an empty list
+  if (list == NULL) {
+    return new_guest;
+  }
+  // traverse to the end
+  struct guest *p;
+  for (p = list; p->next != NULL; p = p->next);
+  // and append the guest to the list
+  p->next = new_guest; 
+  // and return the head
+  return list; 
+} 
+
+struct guest * remove_guest(struct guest *list) {
+  char removing_last[NAME_LEN + 1];
+  char removing_first[NAME_LEN + 1]; 
+  char removing_phone[PHONE_LEN + 1]; 
+
+  printf("Enter phone number: ");
+  read_line(removing_phone, PHONE_LEN);
+  printf("Enter guest's last name: ");
+  read_line(removing_last, NAME_LEN);
+  printf("Enter guest's first name: ");
+  read_line(removing_first, NAME_LEN);
+
+  struct guest *prev, *curr;
+  prev = NULL;
+  curr = list; 
+  for (; curr != NULL; curr = curr->next) {
+    if ((strcmp(removing_phone, curr->phone) == 0) && (strcmp(removing_last, curr->last) == 0) && (strcmp(removing_first, curr->first) == 0)) {
+      // if same, proceed to removal
+      // if removing the first element, then return the next element as the list
+      if (curr == list) {
+        struct guest * next;
+        if (curr->next) {
+          next = curr->next; 
+        } else {
+          next = NULL; 
+        }
+        free(curr);
+        return next; 
+      }
+      prev->next = curr->next;
+      free(curr);
+      return list; 
+    }
+    prev = curr; 
+  }
+  printf("guest does not exist");
+  return list; 
+};
+
+void print_list(struct guest *list){
+  // loops through the list and prints out according to format
+  struct guest *p; 
+  for (p = list; p != NULL; p = p->next) {
+    printf("%-15s%-20s%-20s%5d\n", p->phone, p->last, p->first,  p->party_size);
+  }
+}
+
+void clear_list(struct guest *list){
+ // declare the starting node
+  struct guest *p; 
+  // while there are still elements in the list
+  while (list != NULL) {
+    // first, set the pointer to the current first element of the list
+    p = list; 
+    // move the list forward entirely,  
+    // leaving p so that we don't get a dangling pointer
+    list = list->next; 
+
+    // if p is not a NULL ptr, then go ahead and free it up
+    if (p != NULL) {
+      free(p);
+    }
+  }
+}
+
+int read_line(char str[], int n)
+{
+  int ch, i = 0;
+
+  while (isspace(ch = getchar()))
+    ;
+  str[i++] = ch;
+  while ((ch = getchar()) != '\n') {
+    if (i < n)
+      str[i++] = ch;
+    
+   }
+   str[i] = '\0';
+   return i;
+}
